e9e08866c5902a5b1ed119fae5d8fa20bcd581bc
LeonadoLnex <Leonardolnex@gmail.com>
2026-03-31 11:31:37 -0400

    proyectos sistemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/portafolio`,
+      url: `${baseUrl}/proyectos`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
